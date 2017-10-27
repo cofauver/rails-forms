a3dd22671ccece8ff5e9ad7d50c4f313f18b6091
Cory Fauver <cofauver@gmail.com>
2017-10-26 17:46:37 -0700

    builds fist pass at generate method

--- app/models/token.rb
+++ app/models/token.rb
@@ -1,3 +1,18 @@
 class Token < ApplicationRecord
   belongs_to :user
+
+  def self.generate (user)
+    if  user.class == User
+      puts 'success'
+      nonce = SecureRandom.urlsafe_base64(32)
+      Token.create({user: user, nonce:nonce})
+    else
+
+    end
+  end
+
+  def self.consume
+    puts 'consume method'
+  end
+
 end
