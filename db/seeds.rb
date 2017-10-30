055d8e5b502b1993d2d7c650336702ac2ea78758
Cory Fauver <cofauver@gmail.com>
2017-10-29 22:34:38 -0700

    creates seed data using faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+User.delete_all
+Token.delete_all
+
+10.times do
+  user_data = {name: Faker::Name.name, email: Faker::Internet.email}
+  user = User.create user_data
+  2.times do 
+    Token.generate user
+  end
+end
